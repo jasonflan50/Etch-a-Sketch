de250697700b22c24fbcca204d577fd01ba6ba3e
jasonflan50 <jasonflan50@gmail.com>
2022-01-26 00:08:49 -0500

    Update slider value

--- theScript.js
+++ theScript.js
@@ -50,16 +50,16 @@ const slider = document.getElementById("myRange");
 const output = document.getElementById("demo");
 output.innerHTML = slider.value;
 
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
 
 const resize = document.getElementById('resize').addEventListener('click', () => {
     output.innerHTML = slider.value;
     resetGrid();
     resizeGrid(slider.value);
 })
-/*slider.oninput = function() {
-  output.innerHTML = this.value;
-  resizeGrid(this.value);
-}*/
+
 
 
 
